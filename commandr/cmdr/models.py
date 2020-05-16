feb0840af1190bc9e92aaa2dc7f13873ad5cc13f
rj7shakya <shakyarajad1@gmail.com>
2020-05-16 12:02:07 +0545

    db app final

--- commandr/cmdr/models.py
+++ commandr/cmdr/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+
 class Cmdr(models.Model):
-  text = models.TextField()
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
--- commandr/cmdr/views.py
+++ commandr/cmdr/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+from .models import Cmdr
+
+
+class HomePageViewer(ListView):
+    model = Cmdr
+
+    template_name = 'home.html'
